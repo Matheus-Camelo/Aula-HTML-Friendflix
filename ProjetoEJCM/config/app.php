b32322c5f82a9c1a7770dd8d16a664021b93892c
Matheus Camelo <mtcamelow@gmail.com>
2020-01-29 11:53:53 -0300

    ++Validação na UserRequest|Matheus_validação_back

--- ProjetoEJCM/config/app.php
+++ ProjetoEJCM/config/app.php
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        LaravelLegends\PtBrValidator\ValidatorProvider::class
+        LaravelLegends\PtBrValidator\ValidatorProvider::class //so apagra isos aqui dps so to fazenod pra oclocar no git
 
     ],
 
